d4521c235dae0e7c82a4041aff88311f7ead6773
Katelyn Pace <katelynpace@interapts-macbook-pro-4.local>
2018-08-27 15:01:17 -0500

    Add all files.

--- Guessing Game/main.swift
+++ Guessing Game/main.swift
@@ -8,5 +8,65 @@
 
 import Foundation
 
-print("Hello, World!")
+var playAgain = false
+
+repeat{
+var howManyGuesses = 5
+let randomNumber = Int(arc4random_uniform(99)) + 1
+var guessedNumber: Int? = nil
+
+
+print("Guess a number between 1-100")
+repeat {
+repeat {
+    let input = readLine()!
+    
+    
+    if input == "I know all your secrets" {
+        print("shhh the answer is \(randomNumber)")
+    }
+    
+    if let number = Int(input) {
+        guessedNumber = number
+    } else {
+        print("please enter a number")
+        guessedNumber = nil
+    }
+} while guessedNumber == nil
+
+
+guard let guessedNumber = guessedNumber else {
+    fatalError()
+}
+
+
+if guessedNumber == randomNumber {
+        print("you're correct")
+    break
+    }
+if howManyGuesses == 1 {
+        print("YOU LOST, the right number is \(randomNumber)!")
+        break
+    }
+
+if guessedNumber < randomNumber {
+    print("incorrect, guess higher")
+    howManyGuesses -= 1
+}
+if guessedNumber > randomNumber {
+    print("incorrect, guess lower")
+    howManyGuesses -= 1
+}
+print("you have \(howManyGuesses) left")
+   
+} while howManyGuesses > 0
+    
+    print("Would you like to try again? y or n")
+    let input = readLine()!
+    playAgain = input.lowercased() == "y"
+
+} while playAgain
+
+
+
 
